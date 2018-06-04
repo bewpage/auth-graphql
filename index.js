cb800fcb58ca4d7add2cbcf2b62ccf974a43e535
bewpage <b.jedlinski@bewpage.com>
2018-06-04 16:32:12 +0200

    update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').load();
 const app = require('./server/server');
 
-app.listen(process.env.PORT || 4000, () => {
+app.listen(process.env.PORT, () => {
    console.log('Listening on port 4000');
    console.log('The value of PORT is:', process.env.PORT);
    console.log('The value of PORT is:', process.env.MONGODB_URI);
--- server/server.js
+++ server/server.js
@@ -8,17 +8,15 @@ const passportConfig = require('./services/auth');
 const MongoStore = require('connect-mongo')(session);
 const schema = require('./schema/schema');
 const MONGO_AUTH = require('./mongodb-auth-open');
-const aws = require('aws-sdk');
+require('dotenv').load();
 
 // Create a new Express application
 const app = express();
 
 // Replace with your mongoLab URI
-let MONGODB = new aws.MONGODB({
-    URI: process.env.MONGODB_URI
-});
-const MONGODB_URI = MONGO_AUTH || MONGODB.URI;
-console.log('test from server', MONGODB_URI);
+
+const MONGODB_URI = MONGO_AUTH || MONGODB_URI;
+console.log('test from server', process.env.MONGODB_URI);
 // Mongoose's built in promise library is deprecated, replace it with ES2015 Promise
 mongoose.Promise = global.Promise;
 
